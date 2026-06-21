0d2fc50f6721256a3221a42ab9f2fe407ba0f114
Nisala Jurianze <nisalajurianze@gmail.com>
2026-06-21 20:16:57 +0530

    Loding updated

--- ecommerce/apps/store-client/src/components/layout/LogoLoader.tsx
+++ ecommerce/apps/store-client/src/components/layout/LogoLoader.tsx
@@ -1,7 +1,9 @@
+import favicon from '../../../../public/favicon.svg';
+
 export const LogoLoader = ({ fullHeight = false }: { fullHeight?: boolean }): JSX.Element => (
   <div className={`flex w-full items-center justify-center ${fullHeight ? 'min-h-[calc(100svh)] bg-[#07111f]' : 'min-h-[400px] py-12'}`}>
     <img
-      src="/favicon.svg"
+      src={favicon}
       alt="Loading..."
       className="h-20 w-20 animate-logo-pulse"
     />
